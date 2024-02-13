055a20192d619ee7aa6101b591c17a9688e76bdd
Ciaran Reed <gfgt24@durham.ac.uk>
2024-02-13 11:52:59 +0000

    Added U functionality, took data for different L around KbTc

--- Milestone/Milestone/Milestone.cpp
+++ Milestone/Milestone/Milestone.cpp
@@ -4,8 +4,12 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
+#include <algorithm>
+#include <functional>
 using namespace std;
 using namespace std::chrono;
+using std::transform;
+
 //constants
 
 //const int range =200;
@@ -28,10 +32,31 @@ using namespace std::chrono;
 // whichever system has lowest potential (energy) is what it tends to
 
 
-
-
-double findMean(vector<double> arr, int cutoff, bool beforeCutoff) //cutoff inclusive, eg cutoff 3 means up to and including the element 3
+void saveToFile(string filename, vector <double> xAxis, vector <double> yAxis,vector <double> yErrors, int dataPoints, string xLabel, string yLabel)
 {
+    ofstream file(filename);
+    file << dataPoints;
+    file << "\n";
+    file << xLabel;
+    file << "\n";
+    file << yLabel;
+
+    for (int i = 0; i < dataPoints; i++)
+    {
+        file << "\n";
+        file << xAxis[i];
+        file << ",";
+        file << yAxis[i];
+        file << ",";
+        file << yErrors[i];
+    }
+    file.close();
+}
+
+double findMean(vector<double> arr, int cutoff, bool beforeCutoff) //cutoff inclusive, eg cutoff 3 means up to and including the index 3
+{
+    //cutoff is indec for the last item to be included ( if true)
+    //therefore to do whole array need to set cutoff as array.size() - 1
     double mean = 0;
     if (beforeCutoff)
     {
@@ -86,6 +111,14 @@ double findStdError(vector<double> arr, int cutoff, bool before)
     return findStdDeviation(arr, cutoff, before) / cutoff;
 }  //assumes whole array
 
+void produceGraphs(string dataFileName)
+{
+    string filename = "ProduceGraphs.py ";
+    string command = "Python ";
+    command += filename;
+    command += dataFileName;
+    system(command.c_str());
+}
 
 class particle
 {
@@ -158,6 +191,10 @@ private:
     double J = 0;
     double B = 0;
     double kbt = 0;
+    double K = 0;
+    double U = 0;
+    double DUDK = 0;
+    double E = 0;
     int range = 0;
     int Nsweeps = 0;
     int sweepsPerMag = 0;
@@ -165,7 +202,9 @@ private:
     int dataPoints = 0;
     double meanMag = 0;
     double stdError = 0;
-    vector<int> sweepsHistory;
+    int cutoffValue = 0;
+    vector<double> energyHistory;
+    vector<double> sweepsHistory;
     vector<double> magnetisationHistory;
     vector<particle> lattice;
 
@@ -180,10 +219,13 @@ public:
         Nsweeps = Nsweepsin;
         sweepsPerMag = sweepsPerMagin;
         particleN0 = pow(range, 2);
+        K = J / kbt;
+        U = 0;
         dataPoints = 1 + Nsweeps / sweepsPerMag;
         lattice.resize(particleN0);
         sweepsHistory.resize(dataPoints);
         magnetisationHistory.resize(dataPoints);
+        energyHistory.resize(dataPoints);
         for (int i = 0; i < particleN0; i++)
         {
             lattice[i] = particle(i,range);
@@ -197,6 +239,7 @@ public:
             {
                 magnetisationHistory[i / sweepsPerMag] = calculateMagnetisation();
                 sweepsHistory[i / sweepsPerMag] = i;
+                energyHistory[i / sweepsPerMag] = calculateEnergy();
             }
         }
         findResults();
@@ -214,8 +257,10 @@ public:
         double totalSpin = 2 * counter - particleN0;
         return totalSpin / particleN0;
     }
-    vector<particle> performSweep()
+    double calculateEnergy()
     {
+        //note energy should be = 0 if perfectly allinged, and 1 if not for each site
+        double energy = 0;
         for (int i = 0; i < particleN0; i++)
         {
             particle& info = lattice[i];
@@ -233,6 +278,34 @@ public:
                 }
             }
             //count is sum of adjacent spins
+            //using 4 - here, so that if all count and spin line up the energy contribution is 0
+            double energyContribution = 4 - (count * info.spin);
+            energy += energyContribution;
+        }
+        return energy;
+    }
+    vector<particle> performSweep()
+    {
+        
+        for (int i = 0; i < particleN0; i++)
+        {
+            particle& info = lattice[i];
+
+            int count = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                if (lattice[info.neighbours[j]].spin == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            //count is sum of adjacent spins
+            double energyContribution = 1 - count * info.spin;
+          
             double partitionContribution = exp((2 * (J * count + B) * (-1 * info.spin)) / kbt);
             if (partitionContribution > 1)
             {
@@ -247,6 +320,7 @@ public:
                 }
             }
         }
+        
         return lattice;
     }
     void printHistory()
@@ -259,51 +333,76 @@ public:
             cout << magnetisationHistory[i];
         }
     }
-    void saveToFile(string filename)
+    void findCutoff()
     {
-        ofstream file(filename);
-        file << dataPoints;
-        for (int i = 0; i < dataPoints; i++)
-        {
-            file << "\n";
-            file << sweepsHistory[i];
-            file << ",";
-            file << magnetisationHistory[i];
-        }
-        file.close();
-    }
-    int findCutoff()
-    {
+        int minimumCutoff = Nsweeps - 100;
+  
         vector<double> mean(dataPoints);
         vector<double> stdDev(dataPoints);
-        int cutoffValue = 0;
         mean[0] = magnetisationHistory[0];
         stdDev[0] = 0;
-        for (int i = 1; i < dataPoints; i++) //for every data point after the first one
+        for (int i = 30; i < dataPoints; i++) //for every data point after the 30th one
         {
             mean[i] = findMean(magnetisationHistory, i,true); //up to and incuding the index i
             stdDev[i] = findStdDeviation(magnetisationHistory, i,true);
-            if (stdDev[i] < stdDev[i - 1] && cutoffValue == 0)
+            if (i >= minimumCutoff && stdDev[i] < stdDev[i - 1] && stdDev[i] < stdDev[i - 2] && stdDev[i] < stdDev[i - 3] &&cutoffValue == 0)
             {
-                cutoffValue = i + 5;
-                return cutoffValue;
+                cutoffValue = i + 1;
             }
         }
     }
     void findResults()
     {
-        int cutoffValue = findCutoff();  //cutoff is inclusive, eg 17 would use sweep 17 to the end, so for 20 sweeps (21 data points) cutoff 17 would use index 17 to 20 giving 4 data points
+        //findCutoff();  //cutoff is inclusive, eg 17 would use sweep 17 to the end, so for 20 sweeps (21 data points) cutoff 17 would use index 17 to 20 giving 4 data points
+        cutoffValue = 100;
         meanMag = findMean(magnetisationHistory, cutoffValue, false);
         double stdDev = findStdDeviation(magnetisationHistory, cutoffValue, false);
         double usableData = dataPoints - cutoffValue;
         stdError = stdDev / (usableData);
+        findU();
     }
+
+    void findU()
+    {   
+        vector<double> m2 (dataPoints);
+        vector<double> m4 (dataPoints);
+        vector<double> m2E(dataPoints);
+        vector<double> m4E(dataPoints);
+
+
+        for ( int i = 0; i < dataPoints; i++)
+        {
+            m2[i] = magnetisationHistory[i] * magnetisationHistory[i];
+            m4[i] = pow(magnetisationHistory[i], 4);
+            m2E[i] = m2[i] * energyHistory[i];
+            m4E[i] = m4[i] * energyHistory[i];
+        }
+        double meanM2 = findMean(m2, dataPoints -1, true);
+        double meanM4 = findMean(m4, dataPoints - 1, true);
+        double meanM2E = findMean(m2E, dataPoints - 1, true);
+        double meanM4E = findMean(m4E, dataPoints - 1, true);
+        double meanE = findMean(energyHistory, dataPoints - 1, true);
+        U = 1 - ( (static_cast<double>(1)/3) * (meanM4 / pow(meanM2,2) ) );
+        DUDK = (static_cast<double>(1) - U) * (meanE - (2 * (meanM2E / meanM2)) + (meanM4E / meanM4));
+    }
+
+    double getU()
+    {
+        return U;
+    }
+
     void printResults()
     {
+        cout << "\n Cutoff Value = ";
+        cout << cutoffValue;
         cout << "\n final mean = ";
         cout << meanMag;
         cout << "\n final std error = ";
         cout << stdError;
+        cout << "\n U = ";
+        cout << U;
+        cout << "\n DUDK = ";
+        cout << DUDK;
     }
     double getMeanMag()
     {
@@ -313,30 +412,107 @@ public:
     {
         return stdError;
     }
+    void save(string filename)
+    {
+        vector<double> errors(dataPoints);
+        for (int i = 0; i < dataPoints; i++)
+        {
+            errors[i] = 0;
+        }
+        saveToFile(filename, sweepsHistory, energyHistory,errors, dataPoints,"Number of sweeps","Total Energy");
+    }
+};
+
+class batchSimulation
+{
+private:
+    double meanMag = 0;
+    double stdMagError = 0;
+    double meanU = 0;
+    double stdUError = 0;
+public :
+    batchSimulation() {};
+    batchSimulation(int Nsims, double J, double B, double kbt, int range, int Nsweeps, int sweepsPerMag)
+    {
+        bool valid = false;  
+        while (!valid)
+        {
+            vector<double> magnetisations(Nsims);
+            vector<double> Uvalues(Nsims);
+            vector<simulation> sims(Nsims);
+            
+            for (int i = 0; i < Nsims; i++)
+            {
+                sims[i] = simulation(J, B, kbt, range, Nsweeps, sweepsPerMag);
+                magnetisations[i] = abs(sims[i].getMeanMag());
+                Uvalues[i] = sims[i].getU();
+            }
+
+            meanMag = findMean(magnetisations, Nsims - 1, true);
+            stdMagError = findStdError(magnetisations, Nsims - 1, true);
+            meanU = findMean(Uvalues, Nsims - 1, true);
+            stdUError = findStdError(Uvalues, Nsims - 1, true);
+            //if (stdMagError < 0.05)  //note 0.05 gives close values,  Ad if to ensure data points are close
+            //{
+            valid = true;
+            //}
+        }
+       
+    }
+    double getMeanMag()
+    {
+        return meanMag;
+    }
+    double getStdMagError()
+    {
+        return stdMagError;
+    }
+    double getStdUError()
+    {
+        return stdUError;
+    }
+    double getMeanU()
+    {
+        return meanU;
+    }
+    
 };
 
 int main()
 {
     auto start = high_resolution_clock::now();
     srand((unsigned int)time(NULL));
-    //simulation ( J, B, Kbt, Range, Sweeps, Sweeps per magnetisaton calculation)
-    int Nsims = 10;
-    vector<simulation> allSims(Nsims);
-    vector<double> magnetisations(Nsims);
-    vector<double> stdErrors(Nsims);
-    for (int i = 0; i < Nsims; i++)
+   
+    /*
+    simulation sim1 = simulation(1,0,2.26,40,2000,10);
+    sim1.printResults();
+    sim1.save("criticalExponent.txt");
+    */
+
+
+    
+    int Nbatches = 50;
+    int simsPerBatch = 5;
+    int L = 50;
+    vector<batchSimulation> allBatches(Nbatches);
+    vector<double> magnetisations(Nbatches);
+    vector<double> stdMagErrors(Nbatches);
+    vector<double> UValues(Nbatches);
+    vector<double> KbtValues(Nbatches);
+    vector<double> stdUErrors(Nbatches);
+    for (int i = 0; i < Nbatches; i++)
     {
-        allSims[i] = simulation(0.5, -0.05, 1, 100, 100, 1);
-        allSims[i].printResults();
-        magnetisations[i] = allSims[i].getMeanMag();
-        stdErrors[i] = allSims[i].getStdError();
+        KbtValues[i] = 2 + 0.008 * i;
+        allBatches[i] = batchSimulation(simsPerBatch,1, 0, KbtValues[i], L, 3000, 20);
+        magnetisations[i] = allBatches[i].getMeanMag();
+        stdMagErrors[i] = allBatches[i].getStdMagError();
+        UValues[i] = allBatches[i].getMeanU();
+        stdUErrors[i] = allBatches[i].getStdUError();
     }
-    double totalMeanMag = findMean(magnetisations, Nsims -1 , true);
-    double totalStdErorr = findStdDeviation(magnetisations, Nsims -1 , true);
-    cout << "\n Total Mean Mag = ";
-    cout << totalMeanMag;
-    cout << "\n total Std Error = ";
-    cout << totalStdErorr;
+    saveToFile("VaryK_L=50.txt", KbtValues, UValues,stdUErrors, Nbatches, "Kbt", "U(Kbt,L)");
+    
+    //produceGraphs("batchsim3.txt");
+    
 
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
@@ -344,20 +520,7 @@ int main()
     cout << duration.count();
     cout << " microsec " << endl;
 
-
-    allSims[1].saveToFile("magnetisation data.txt");
-
-    string filename = "ProduceGraphs.py";
-    string command = "Python ";
-    command += filename;
-    system(command.c_str());
-
     cout << "\n";
-
-
+   
     system("pause");
-  
 }
-//cutoff = 4
-//mean = -0.34368
-//std dev = 
--- Milestone/Milestone/ProduceGraphs.py
+++ Milestone/Milestone/ProduceGraphs.py
@@ -1,31 +1,58 @@
 import numpy
 import matplotlib.pyplot as plt
 import os
+import sys
+import math
 
+def onsager(J):
+    kbt = 1
+    zcrit = -1 + math.sqrt(2)
+    z = math.exp((-2*J)/kbt)
+    if (z < zcrit):
+        M = ((1+(z**2))**0.25)*((1-6*(z**2)+(z**4))**0.125)/((1-(z**2))**0.5)
+        return M
+    else:
+        z = 2* zcrit - z
+        M = 1-  ((1+(z**2))**0.25)*((1-6*(z**2)+(z**4))**0.125)/((1-(z**2))**0.5)
+        return M
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, "magnetisation data.txt")
+file_path = os.path.join(script_dir, sys.argv[1])
 f = open(file_path,"r")
 Ndata = int(f.readline())
-shistory = numpy.empty(Ndata)
-mhistory = numpy.empty(Ndata)
+xLabel = f.readline()
+yLabel = f.readline()
+xs = numpy.empty(Ndata)
+ys = numpy.empty(Ndata)
+yErrors = numpy.empty(Ndata)
 for i in range (0, Ndata):
     data = f.readline()
-    shistory[i], mhistory[i] = data.split(",")
-
-
+    xs[i], ys[i],yErrors[i] = data.split(",")
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
-ax.spines['bottom'].set_position('center')
+#ax.spines['bottom'].set_position('center')
 ax.spines['left'].set_position(('data', 0))
-ax.set_ylim([-1,1])
-ax.set_ylabel("Magnetisation")
-ax.set_xlabel("Number of Sweeps")
+ax.spines["left"].set_color("white")
+ax.spines["bottom"].set_color("white")
+#ax.set_ylim([-1,1])
+#ax.set_ylabel(yLabel)
+ax.set_ylim([0,1])
+ax.set_ylabel("Absolute Magnetisation",color = "white")
+ax.set_xlabel(xLabel,color = "white")
+ax.tick_params(axis='x', colors='white') 
+ax.tick_params(axis='y', colors='white') 
+plt.errorbar(xs,ys,yerr = yErrors,fmt = "xr" ,ecolor = "r",capsize = 2,label = "Simulation")
 
-plt.plot(shistory,mhistory)
+
+analytical = numpy.empty(Ndata)
+for i in range(0,Ndata):
+    analytical[i] = onsager(xs[i])
+plt.plot(xs,analytical,color = "g",label ="Analytical")
+plt.legend(loc='center right')
+fig.savefig("VaryJ.png",transparent = "True")
 plt.show()
 
 
